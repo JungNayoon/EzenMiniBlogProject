d635bda509bc47e7a50cf2178dc620a3575f2e56
Auden Kim <sasameyuki03@naver.com>
2023-04-25 19:19:59 +0900

    first commit

---
+++ afterproject/src/main/java/com/miniblog/afterproject/AfterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.miniblog.afterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterprojectApplication.class, args);
+	}
+
+}
---
+++ afterproject/src/test/java/com/miniblog/afterproject/AfterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniblog.afterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
