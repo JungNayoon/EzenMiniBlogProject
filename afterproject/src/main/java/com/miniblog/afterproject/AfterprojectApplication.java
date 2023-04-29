fab1613ab2db8e1658239f77ad1d9a964259c9b7
Auden Kim <sasameyuki03@naver.com>
2023-04-29 13:15:12 +0900

    2023-04-29 (재국) 팀원 프로젝트 세팅 공유. 아직 남음

--- afterproject/src/main/java/com/miniblog/afterproject/AfterprojectApplication.java
+++ afterproject/src/main/java/com/miniblog/afterproject/AfterprojectApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AfterprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AfterprojectApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AfterprojectApplication.class, args);
+  }
 }
